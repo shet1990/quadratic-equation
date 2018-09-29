1f8b1db209e6690a0bb13d1010b6d54c24dc801f
shet1990 <shet1990@gmail.com>
2018-09-29 23:55:28 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const solutions = [];
+
+    let firstPositionX = equation.indexOf(" * x^2");
+    let secondPositionX = equation.indexOf("* x ");
+    let a = equation.slice(0, firstPositionX);
+    let b;
+    let c;
+
+    if (equation.slice(firstPositionX + 7, firstPositionX + 8) == '+') {
+        b = equation.slice(firstPositionX + 9, secondPositionX);
+    } else {
+        b = -equation.slice(firstPositionX + 9, secondPositionX);
+    }
+    if (equation.slice(secondPositionX + 4, secondPositionX + 5) == '+') {
+        c = equation.slice(secondPositionX + 6);
+    } else {
+        c = -equation.slice(secondPositionX + 6);
+    }
+
+    let discrim = b * b - (4 * a * c);
+    let firstX = Math.round(0.5 * (-b + Math.sqrt(discrim)) / a);
+    let secondX = Math.round(0.5 * (-b - Math.sqrt(discrim)) / a);
+
+    solutions.push(firstX);
+    solutions.push(secondX);
+    solutions.sort((a, b) => a - b);
+
+    return solutions;
 }
